fa1edf8aa6f5604171bbbb17d7c469cda7749775
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-05-14 16:58:00 +0800

    Update Notes jsx

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -50,6 +50,7 @@ const Notes = ({ notes }) => {
         </button>
       </header>
       <div className="notes__container">
+        {filteredNotes.length === 0 ? <p className="empty__notes">No Notes Found 🗿</p> : ""}
         {filteredNotes.map((note) => (
           <NoteItem key={note.id} note={note} />
         ))}
